9821feaeaf1a09629b2f7c87b191bc66a486a55b
flyinkilla <62249392+flyinkilla@users.noreply.github.com>
2020-03-24 17:35:26 +0300

    Рассчёт растояния между двумя точками в классе main

--- sandbox/src/main/java/ru/stqa/javaLearn/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javaLearn/sandbox/MyFirstProgram.java
@@ -2,8 +2,18 @@ package ru.stqa.javaLearn.sandbox;
 
 public class MyFirstProgram {
 
+    static double distance;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Point x = new Point(1.0, 2.0);
+        Point y = new Point(3.0, 4.0);
+        distance(x, y);
+        System.out.println(distance);
+    }
+
+    public static void distance(Point p1, Point p2) {
+
+        distance = Math.sqrt(Math.pow((p1.p2 - p1.p1), 2) + Math.pow((p2.p2 - p2.p1), 2));
     }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/javaLearn/sandbox/Point.java
@@ -0,0 +1,13 @@
+package ru.stqa.javaLearn.sandbox;
+
+public class Point {
+
+    public double p1;
+    public double p2;
+
+    public Point(double p1, double p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+}
